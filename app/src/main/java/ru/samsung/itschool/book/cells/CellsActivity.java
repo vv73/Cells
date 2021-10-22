7a8f8841f73c24afbb28903e901fc75ebd9f1880
Tatarskiy Damirka <damir-developer@yandex.ru>
2021-10-23 00:25:54 +0400

    version without diagonal

--- app/src/main/java/ru/samsung/itschool/book/cells/CellsActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/cells/CellsActivity.java
@@ -1,5 +1,6 @@
 package ru.samsung.itschool.book.cells;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ public class CellsActivity extends Activity implements OnClickListener,
 
     private int WIDTH = 10;
     private int HEIGHT = 10;
+    private int cnt = 0;
 
     private Button[][] cells;
 
@@ -34,16 +36,9 @@ public class CellsActivity extends Activity implements OnClickListener,
     }
 
     void generate() {
-
-        //Эту строку нужно удалить
-        Task.showMessage(this, "Добавьте код в функцию активности generate() для генерации клеточного поля");
-
-
-        for (int i = 0; i < HEIGHT; i++)
-            for (int j = 0; j < WIDTH; j++) {
-                //ADD YOUR CODE HERE
-                //....
-
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                cells[i][j].setText("-");
             }
     }
 
@@ -56,23 +51,119 @@ public class CellsActivity extends Activity implements OnClickListener,
 
     @Override
     public void onClick(View v) {
-        //Эту строку нужно удалить
-        Stub.show(this, "Добавьте код в функцию активности onClick() - реакцию на нажатие на клетку");
 
         Button tappedCell = (Button) v;
 
         //Получаем координтаты нажатой клетки
         int tappedX = getX(tappedCell);
         int tappedY = getY(tappedCell);
-        //ADD YOUR CODE HERE
-        //....
 
+            setText(tappedX, tappedY);
     }
 
-	/*
+
+    public void setText(int x, int y) {
+        if (cnt % 2 == 0) {
+            if (cells[x][y].getText().toString() == "-") {
+                cells[x][y].setText("X");
+                cnt++;
+            }
+        } else {
+            if (cells[x][y].getText().toString() == "-") {
+                cells[x][y].setText("0");
+                cnt++;
+            }
+        }
+        if (checkWin(x, y)) {
+            youWin();
+        }
+        if (cnt == 9){
+            Stub.show(this, "Ничья!");
+            cnt = 0;
+            makeCells();
+            generate();
+        }
+    }
+
+    public void youWin() {
+        Stub.show(this, "Победа!");
+        cnt = 0;
+        makeCells();
+        generate();
+    }
+
+    public boolean checkd(int x, int y, String check) {
+        System.out.println(check);
+        System.out.println("x: " + (x) + " y: " + (y));
+        if (((String) cells[x][y].getTag()).split(",")[1] == "1") {
+            if (((String) cells[x][y].getTag()).split(",")[0] == "1") {
+                if ((cells[x + 1][y - 1].getText().toString() == check && cells[x - 1][y + 1].getText().toString() == check)
+                        || (cells[x + 1][y + 1].getText().toString() == check && cells[x - 1][y - 1].getText().toString() == check)) {
+                    return true;
+                }
+            }
+        }
+        if (((String) cells[x][y].getTag()).split(",")[1] == "0") {
+            if (((String) cells[x][y].getTag()).split(",")[0] == "2") {
+                if (cells[x - 1][y + 1].getText().toString() == check && cells[x - 2][y + 2].getText().toString() == check) {
+                    return true;
+                }
+            }
+            if (((String) cells[x][y].getTag()).split(",")[0] == "0") {
+                if (cells[x + 1][y + 1].getText().toString() == check && cells[x + 2][y + 2].getText().toString() == check) {
+                    return true;
+                }
+            }
+        }
+        if (((String) cells[x][y].getTag()).split(",")[1] == "2") {
+            if (((String) cells[x][y].getTag()).split(",")[0] == "2") {
+                if (cells[x - 1][y - 1].getText().toString() == check && cells[x - 2][y - 2].getText().toString() == check) {
+                    return true;
+                }
+            }
+            if (((String) cells[x][y].getTag()).split(",")[0] == "0") {
+                if (cells[x + 1][y - 1].getText().toString() == check && cells[x + 2][y - 2].getText().toString() == check) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean checkWin(int x, int y) {
+        String check = "";
+        int xx = 0, yy = 0;
+        if (cells[x][y].getText().toString() == "X") {
+            check = "X";
+        } else {
+            check = "0";
+        }
+        if (checkd(x, y, check)) {
+            return true;
+        }
+        for (int ix = 0; ix < 3; ix++) {
+            if (cells[ix][y].getText().toString() == check) {
+                xx++;
+            }
+        }
+        if (xx == 3) {
+            return true;
+        }
+        for (int iy = 0; iy < 3; iy++) {
+            if (cells[x][iy].getText().toString() == check) {
+                yy++;
+            }
+        }
+        if (yy == 3) {
+            return true;
+        }
+        return false;
+    }
+    /*
      * NOT FOR THE BEGINNERS
-	 * ==================================================
-	 */
+     * ==================================================
+     */
 
     int getY(View v) {
         return Integer.parseInt(((String) v.getTag()).split(",")[1]);
@@ -86,9 +177,9 @@ public class CellsActivity extends Activity implements OnClickListener,
         cells = new Button[HEIGHT][WIDTH];
         GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
         cellsLayout.removeAllViews();
-        cellsLayout.setColumnCount(WIDTH);
-        for (int i = 0; i < HEIGHT; i++)
-            for (int j = 0; j < WIDTH; j++) {
+        cellsLayout.setColumnCount(3);
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
                 LayoutInflater inflater = (LayoutInflater) getApplicationContext()
                         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
